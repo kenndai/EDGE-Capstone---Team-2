7a6ebb7d561ccd809013f858cdd2f23cbea89b1b
kennydai <knnydai@gmail.com>
2023-01-25 22:47:05 +0000

    finished account_repo

--- Accounts/Repositories/account_repository.py
+++ Accounts/Repositories/account_repository.py
@@ -1,20 +1,68 @@
 # import psycopg2
+from unittest import result
 from dbconnect import cursor
+import psycopg2
 from Accounts.Models.account import Account
+from Accounts.Models.customer import Customer
+
 
 class AccountRepository():
     def insert(self, account: Account) -> Account:
-        # with psycopg2.connect() as db:
-        #     with db.cursor() as cursor:
-        cursor.execute("""
-            INSERT INTO account
-                (AccountNumber, Customer, CurrentBalance) VALUES
-                (%(account_num)s, %(customer_id)s, %(current_balance)s)
-                RETURNING id
-            """, {
-            'account_num': account.account_num,
-            'customer_id': account.customer_id,
-            'current_balance': account.current_balance,
-        })
-        account.account_id = cursor.fetchone()[0]
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO account
+                        (AccountNumber, Customer, CurrentBalance) VALUES
+                        (%(account_num)s, %(customer_id)s, %(current_balance)s)
+                        RETURNING id
+                    """, {
+                    'account_num': account.account_num,
+                    'customer_id': account.customer_id,
+                    'current_balance': account.current_balance,
+                })
+                account.account_id = cursor.fetchone()[0]
         return account
+
+    def get_account_by_number(self, account: Account) -> Account:
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT * FROM account WHERE AccountNumber = %(account_num)s", {
+                    'acccount_num': account.account_num
+                })
+                row = cursor.fetchone()
+        return Account.construct(account_id=row[0], account_num=row[1], customer_id=Customer.construct(customer_id=row[2]), current_balance=round(row[3], 2))
+
+    def get_all(self, account: Account) -> 'list[Account]':
+        result_accounts = []
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT * FROM account")
+                all_accounts = cursor.fetchall()
+
+        for acc in all_accounts:
+            result_accounts.append(Account.construct(account_id=acc[0], account_num=acc[1], customer_id=Customer.construct(customer_id=acc[2]), current_balance=round(acc[3], 2)))
+
+        return result_accounts
+
+    def update_account(self, account: Account) -> None:
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("""
+                    UPDATE account
+                    SET AccountNumber=%(account_num)s, CustomerID=%(custoemr_id)s, CurrentBalance=%(current_balance)s
+                    WHERE ID=%(id)s
+                    """, {
+                        "account_id": account.account_id,
+                        "account_num": account.account_num,
+                        "customer_id": account.customer.id,
+                        "current_balance": account.current_balance
+                    })
+
+    def delete(self, account_id) -> None:
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("DELETE FROM account WHERE ID=%(account_id)s", {
+                    'account_id': account_id
+                })
+
+
--- Accounts/Repositories/customer_repository.py
+++ Accounts/Repositories/customer_repository.py
@@ -1,6 +1,6 @@
 import psycopg2
-from Accounts.Models.customer import customer
-from Accounts.Models.address import address
+from Accounts.Models.customer import Customer
+from Accounts.Models.address import Address
 
 class customer_repository():
     def insert(self, customer: Customer) -> Customer:
@@ -21,27 +21,27 @@ class customer_repository():
                 customer.id = cursor.fetchone()[0]
             return customer
 
-        def get_by_id(self, id) -> Customer:
-            with psycopg2.connect() as db:
-                with db.cursor() as cursor:
-                    cursor.execute("""
-                        SELECT ID, FirstName, LastName, Email, AddressID FROM
-                            customer WHERE ID=%(customer_id)s
-                        """, {
-                        'customer_id': id
-                    }
-                    )
-                    row = cursor.fetchone()
-                        
-            return Customer.construct(id=row[0], first_name=row[1], last_name=row[2], email_address=row[3], address=Address.construct(id=row[4]))
+    def get_by_id(self, id) -> Customer:
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("""
+                    SELECT ID, FirstName, LastName, Email, AddressID FROM
+                        customer WHERE ID=%(customer_id)s
+                    """, {
+                    'customer_id': id
+                }
+                )
+                row = cursor.fetchone()
+                    
+        return Customer.construct(id=row[0], first_name=row[1], last_name=row[2], email_address=row[3], address=Address.construct(id=row[4]))
                 
-        def delete(self, id) -> None:
-            with psycopg2.connect() as db:
-                with db.cursor() as cursor:
-                    cursor.execute("""
-                        DELETE FROM customer WHERE ID=%(customer_id)s
-                        """,{
-                        'customer_id': id
-                    }
-                    )
+    def delete(self, id) -> None:
+        with psycopg2.connect() as db:
+            with db.cursor() as cursor:
+                cursor.execute("""
+                    DELETE FROM customer WHERE ID=%(customer_id)s
+                    """,{
+                    'customer_id': id
+                }
+                )
                             
