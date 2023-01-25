aab80a579f07b481fb0b58cb9302832b93f58352
fggreenaway5519 <francisco.greenaway@capgroup.com>
2023-01-25 22:27:34 +0000

    Added address repository functions

--- Accounts/Repositories/address_repository.py
+++ Accounts/Repositories/address_repository.py
@@ -13,6 +13,26 @@ class AddressRepository():
             'city': address.city,
             'state': address.state,
             'zip_code': address.zip_code
-        })
+        }
+        )
         address.address_id = cursor.fetchone()[0]
         return address
+
+    def get_by_id(self, address_id) -> Address:
+        cursor.execute("""
+            SELECT AddressID, Street, City, State, ZipCode FROM
+                address WHERE AddressID=%(address_id)s
+            """, {
+            'address_id': address_id
+            }
+            )
+        row = cursor.fetchone()
+        return Address.construct(address_id=row[0], street=row[1], city=row[2], state=row[3], zip_code=row[4])
+
+    def delete(self, address_id) -> None:
+        cursor.execute("""
+            DELETE FROM address WHERE AddressID=%(address_id)s
+            """, {
+                'address_id': address_id
+            }
+            )    
